4dbe014f1cf0b77d29d034672470f12990904b3c
Lukas <Lukas@Lukas-PC>
2013-12-08 19:14:36 +0800

    Conversion both ways for 32bit

--- hw6/hw6.c
+++ hw6/hw6.c
@@ -3,17 +3,6 @@
 /*DECLARATIONS*/
 #define DEBUG 1
 #define N 1000
-#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
-#define BYTETOBINARY(byte)  \
-  (byte & 0x80 ? 1 : 0), \
-  (byte & 0x40 ? 1 : 0), \
-  (byte & 0x20 ? 1 : 0), \
-  (byte & 0x10 ? 1 : 0), \
-  (byte & 0x08 ? 1 : 0), \
-  (byte & 0x04 ? 1 : 0), \
-  (byte & 0x02 ? 1 : 0), \
-  (byte & 0x01 ? 1 : 0)
-/* note semi-colon here */
 /*DEFINITIONS*/
 
 struct number {
@@ -123,135 +112,153 @@ void print_bitpattern_float(float d) {
 	printf("\n");
 }
 
-void convert_union_32(char *word) {
-	//char word[64];
-	//scanf("%s", &word);
-	//char word[] = "1101";
-	//char word[] = "00111111110000000000000000000000";
-
-	//fgets(word, 63, stdin);
-	//word[63] = 0;
+void convert_union_32(char *word, int flag_to_binary) {
 	int len = strlen(word);
 
 	printf("Word : %s\n", word);
 	printf("Len : %d\n", len);
 
-	if (len == 32) {
+	if (flag_to_binary) {
 
+		float f;
+		sscanf(word, "%f", &f);
+		union number_union_32 u_float;
+		u_float.f = f;
+		printf("Float is : %f\n", u_float.f);
+		printf("Decimal is : %d\n", u_float.i);
+		printBits(sizeof(u_float.i), &(u_float.i));
+	} else {
+		int i = 0;
+		unsigned long r = 0;
+		char * bin = word;
+		for (i = 0; i < len; i++) {
+			r = r * 2 + (bin[i] == '1' ? 1 : 0);
+		}
+
+		union number_union_32 u_float;
+		u_float.i = r;
+		printf("Float is : %f\n", u_float.f);
+		printf("Decimal is : %d\n", u_float.i);
 	}
-
-	int i = 0;
-	unsigned long r = 0;
-	char * bin = word;
-	for (i = 0; i < len; i++) {
-		r = r * 2 + (bin[i] == '1' ? 1 : 0);
-	}
-	printf("Decimal is : %d\n", r);
-
-	union number_union_32 u_float;
-	u_float.i = r;
-	printf("Float is : %f\n", u_float.f);
-
 	printf("\n");
 }
 
-void convert_union_64(char *word) {
-	//char word[64];
-	//scanf("%s", &word);
-	//char word[] = "1101";
-	//char word[] =	"0011111111111000000000000000000000000000000000000000000000000000";
-
-	//fgets(word, 63, stdin);
-	//word[63] = 0;
+void convert_union_64(char *word, int flag_to_binary) {
 	int len = strlen(word);
 
 	printf("Word : %s\n", word);
 	printf("Len : %d\n", len);
 
-	if (len == 64) {
-
+	if (flag_to_binary) {
+		double d;
+		sscanf(word, "%lf", &d);
+		union number_union_64 u_double;
+		u_double.d = d;
+		printf("Float is : %f\n", u_double.d);
+		printf("Decimal is : %d\n", u_double.l);
+		printBits(sizeof(u_double.l), &(u_double.l));
+	} else {
+		int i = 0;
+		unsigned long r = 0;
+		char * bin = word;
+		for (i = 0; i < len; i++) {
+			r = r * 2 + (bin[i] == '1' ? 1 : 0);
+		}
+		union number_union_64 u_doule;
+		u_doule.l = r;
+		printf("Double is : %f\n", u_doule.d);
+		printf("Decimal is : %lu\n", r);
 	}
-
-	int i = 0;
-	unsigned long r = 0;
-	char * bin = word;
-	for (i = 0; i < len; i++) {
-		r = r * 2 + (bin[i] == '1' ? 1 : 0);
-	}
-	printf("Decimal is : %lu\n", r);
-
-	union number_union_64 u_doule;
-	u_doule.l = r;
-	printf("Double is : %f\n", u_doule.d);
-
 	printf("\n");
 }
 
-void convert_pointer_32(char *word) {
-	//char word[64];
-	//scanf("%s", &word);
-	//char word[] = "1101";
-	//char word[] = "00111111110000000000000000000000";
-
-	//fgets(word, 63, stdin);
-	//word[63] = 0;
+void convert_pointer_32(char *word, int flag_to_binary) {
 	int len = strlen(word);
 
 	printf("Word : %s\n", word);
 	printf("Len : %d\n", len);
 
-	int i = 0;
-	unsigned long r = 0;
-	char *bin = word;
-	for (i = 0; i < len; i++) {
-		r = r * 2 + (bin[i] == '1' ? 1 : 0);
+	if (flag_to_binary) {
+
+		float f;
+		sscanf(word, "%f", &f);
+		//Pointer to float
+		float *p;
+		//long variable
+		uint32_t r = 0;
+		//Make the pointer point to the address where the long r is
+		p = &r;
+		//Change the value where pointer p points (the float f), but treat it as it is unsigned long
+		*p = f;
+
+		printf("Float is : %f\n", f);
+		printf("Decimal is : %d\n", r);
+		printBits(sizeof(r), &(r));
+	} else {
+		int i = 0;
+		unsigned long r = 0;
+		char *bin = word;
+		for (i = 0; i < len; i++) {
+			r = r * 2 + (bin[i] == '1' ? 1 : 0);
+		}
+
+		//Pointer
+		int *p;
+		//Float variable
+		float f;
+		//Make the pointer point to the address where the float f is
+		p = &f;
+		//Change the value where pointer p points (the float f), but treat it as it is unsigned long
+		*p = r;
+
+		printf("Float is : %f\n", f);
+		printf("Decimal is : %d\n", r);
 	}
-	printf("Decimal is : %d\n", r);
-
-	//Pointer
-	int *p;
-	//Float variable
-	float f;
-	//Make the pointer point to the address where the float f is
-	p = &f;
-	//Change the value where pointer p points (the float f), but treat it as it is unsigned long
-	*p = r;
-
-	printf("Float is : %f\n", f);
 	printf("\n");
 }
 
-void convert_pointer_64(char *word) {
-	//char word[64];
-	//scanf("%s", &word);
-	//char word[] = "1101";
-	//char word[] ="0011111111111000000000000000000000000000000000000000000000000000";
-
-	//fgets(word, 63, stdin);
-	//word[63] = 0;
+void convert_pointer_64(char *word, int flag_to_binary) {
 	int len = strlen(word);
 
 	printf("Word : %s\n", word);
 	printf("Len : %d\n", len);
 
-	int i = 0;
-	uint64_t r = 0;
-	char * bin = word;
-	for (i = 0; i < len; i++) {
-		r = r * 2 + (bin[i] == '1' ? 1 : 0);
+	if (flag_to_binary) {
+		double f;
+		sscanf(word, "%f", &f);
+		//Pointer to float
+		double *p;
+		//long variable
+		uint64_t r = 0;
+		//Make the pointer point to the address where the long r is
+		p = &r;
+		//Change the value where pointer p points (the float f), but treat it as it is unsigned long
+		*p = f;
+
+		printf("Double is : %f\n", f);
+		printf("Decimal is : %d\n", r);
+		printBits(sizeof(r), &(r));
+	} else {
+
+		int i = 0;
+		uint64_t r = 0;
+		char * bin = word;
+		for (i = 0; i < len; i++) {
+			r = r * 2 + (bin[i] == '1' ? 1 : 0);
+		}
+		printf("Decimal is : %lu\n", r);
+
+		//Pointer
+		long *p;
+		//Double variable
+		double d;
+		//Make the pointer point to the address where the double d is
+		p = &d;
+		//Change the value where pointer p points (the double d), but treat it as it is unsigned long
+		*p = r;
+
+		printf("Double is : %f\n", d);
 	}
-	printf("Decimal is : %lu\n", r);
-
-	//Pointer
-	long *p;
-	//Double variable
-	double d;
-	//Make the pointer point to the address where the double d is
-	p = &d;
-	//Change the value where pointer p points (the double d), but treat it as it is unsigned long
-	*p = r;
-
-	printf("Double is : %f\n", d);
 	printf("\n");
 }
 
@@ -356,24 +363,36 @@ int main(int argc, char **argv) {
 	char *w = word;
 	//word = w;
 	int flag_from_binary_to_floating = read_input(w, flag_32bit);
-	printf("Word outside :%s\n", w);
+	printf("Word outside :%s\n\n", w);
 
 	if (flag_from_binary_to_floating) {
-
+		printf("flag_from_binary_to_floating TRUE\n");
 		if (flag_32bit) {
 			printf("Pointer - 32bit Float\n");
-			convert_pointer_32(w);
+			convert_pointer_32(w, 0);
 			printf("\n");
 			printf("Union - 32bit Float\n");
-			convert_union_32(w);
+			convert_union_32(w, 0);
 			printf("\n");
 		} else {
 			printf("Pointer - 64bit Float\n");
-			convert_pointer_64(w);
+			convert_pointer_64(w, 0);
 			printf("\n");
 			printf("Union - 64bit Float\n");
-			convert_union_64(w);
+			convert_union_64(w, 0);
 			printf("\n");
+		}
+	} else {
+		printf("flag_from_binary_to_floating FALSE\n");
+		if (flag_32bit) {
+			printf("Pointer - 32bit Float\n");
+			convert_pointer_32(w, 1);
+			printf("\n");
+			printf("Union - 32bit Float\n");
+			convert_union_32(w, 1);
+			printf("\n");
+		} else {
+
 		}
 	}
 
@@ -386,17 +405,17 @@ int main_bin_to_floating(int argc, char **argv) {
 	char word[] = "00111111110000000000000000000000";
 
 	printf("Pointer - 32bit Float\n");
-	convert_pointer_32(&word);
+	convert_pointer_32(&word, 0);
 	printf("\n");
-	//printf("Pointer - 64bit Double\n");
-	//convert_pointer_64();
-	//printf("\n");
+//printf("Pointer - 64bit Double\n");
+//convert_pointer_64();
+//printf("\n");
 	printf("Union - 32bit Float\n");
-	convert_union_32(&word);
+	convert_union_32(&word, 0);
 	printf("\n");
-	//printf("Union - 64bit Double\n");
-	//convert_union_64();
-	//printf("\n");
+//printf("Union - 64bit Double\n");
+//convert_union_64();
+//printf("\n");
 
 	printf("Bit field - 32bit Float\n");
 	convert_bit_field_32();
@@ -417,7 +436,7 @@ int main3(int argc, char **argv) {
 
 	printf("\n");
 	printf("\n");
-	//float i = 1.234f;
+//float i = 1.234f;
 	printBits(sizeof(uf.i), &(uf.i));
 
 	union number_union_64 ud;
@@ -434,20 +453,20 @@ int main3(int argc, char **argv) {
 }
 int main2(int argc, char **argv) {
 	setvbuf(stdout, NULL, _IONBF, 0);
-	//float f1, f2;
+//float f1, f2;
 
-	//int byte = 10;
-	//char byte = 10;
+//int byte = 10;
+//char byte = 10;
 	run_test();
 
 	float in_float;
 	double in_double;
-	//char str[255];
+//char str[255];
 
-	//char str [] = "1.23";
+//char str [] = "1.23";
 	char str[] = "1.23456789";
 	printf("Input 1. float or double number 2. bit pattern :\n>");
-	//scanf("%s", &str);
+//scanf("%s", &str);
 
 	sscanf(str, "%f", &in_float);
 	sscanf(str, "%lf", &in_double);
@@ -455,7 +474,7 @@ int main2(int argc, char **argv) {
 	printf("%15.15e\n%15.15e\n", in_float, in_double);
 	printf("========================\n");
 
-	//in_float = (float) in_double;
+//in_float = (float) in_double;
 	if (in_float == (float) in_double) {
 		printf("%.5e == %e", in_float, in_double);
 	} else {
@@ -464,10 +483,7 @@ int main2(int argc, char **argv) {
 	printf("\n");
 
 	float M = 1.0f;
-	//M = 1.175494350822287500e-38f;
-	//printf("Leading text :"BYTETOBINARYPATTERN, BYTETOBINARY(byte));
-	//printf("M: "BYTETOBINARYPATTERN" "BYTETOBINARYPATTERN"\n",
-	//		BYTETOBINARY(M >> 8), BYTETOBINARY(M));
+
 	print_bitpattern_float(M);
 	printf("\n");
 	union {
@@ -475,7 +491,7 @@ int main2(int argc, char **argv) {
 		float f;
 	} u;
 
-	// Convert floating-point bits to integer:
+// Convert floating-point bits to integer:
 	u.f = 3.14159f;
 	printf("As integer: %08x\n", u.i);
 	u.i = 1234567890;
